a1d6cb18a6c59ad949e13c29058822cdeb43a4fd
ArturBelo <arturbelo2102@gmail.com>
2026-04-30 18:58:22 -0300

    aula08ex03.c

--- aula08ex03.c
+++ aula08ex03.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+
+float potencia(float base, float expoente){
+    if(expoente == 0){
+        return 1;
+    }else{
+        return base * potencia(base, expoente - 1);
+    }
+}
+
+int main(){
+    float base, expoente;
+
+    printf("Digite o numero da base: ");
+    scanf("%f", &base);
+
+    printf("Digite o numero do expoente: ");
+    scanf("%f", &expoente);
+
+    float resultado = potencia(base, expoente);
+
+    printf("Resultado: %.2f\n", resultado);
+
+    return 0;
+}
